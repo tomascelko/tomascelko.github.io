3610f0e90baabaf1178cf9d1c5dd8ecaf952445f
tomascelko <54834854+tomascelko@users.noreply.github.com>
2024-12-09 23:09:25 +0000

    Deploying to gh-pages from @ tomascelko/tomascelko.github.io@71daf687fbd310ed6cd9fe850c07710e87037766 🚀

---
+++ assets/clusterer_core/CMakeFiles/clusterer_static.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clusterer_static.dir/src/cuda_resources/cuda_all.cu.o"
+  "CMakeFiles/clusterer_static.dir/src/cuda_resources/cuda_all.cu.o.d"
+  "CMakeFiles/clusterer_static.dir/src/data_flow/external_dataflow_controller.cpp.o"
+  "CMakeFiles/clusterer_static.dir/src/data_flow/external_dataflow_controller.cpp.o.d"
+  "CMakeFiles/clusterer_static.dir/src/data_nodes/clustering/external_cuda_clusterer.cpp.o"
+  "CMakeFiles/clusterer_static.dir/src/data_nodes/clustering/external_cuda_clusterer.cpp.o.d"
+  "CMakeFiles/clusterer_static.dir/src/data_nodes/io/external_data_reader.cpp.o"
+  "CMakeFiles/clusterer_static.dir/src/data_nodes/io/external_data_reader.cpp.o.d"
+  "CMakeFiles/clusterer_static.dir/src/data_structs/calibration.cpp.o"
+  "CMakeFiles/clusterer_static.dir/src/data_structs/calibration.cpp.o.d"
+  "CMakeFiles/clusterer_static.dir/src/data_structs/cluster.cpp.o"
+  "CMakeFiles/clusterer_static.dir/src/data_structs/cluster.cpp.o.d"
+  "CMakeFiles/clusterer_static.dir/src/data_structs/mm_hit.cpp.o"
+  "CMakeFiles/clusterer_static.dir/src/data_structs/mm_hit.cpp.o.d"
+  "CMakeFiles/clusterer_static.dir/src/data_structs/node_args.cpp.o"
+  "CMakeFiles/clusterer_static.dir/src/data_structs/node_args.cpp.o.d"
+  "CMakeFiles/clusterer_static.dir/src/data_structs/tpx3_hit.cpp.o"
+  "CMakeFiles/clusterer_static.dir/src/data_structs/tpx3_hit.cpp.o.d"
+  "CMakeFiles/clusterer_static.dir/src/data_structs/tpx4_hit.cpp.o"
+  "CMakeFiles/clusterer_static.dir/src/data_structs/tpx4_hit.cpp.o.d"
+  "CMakeFiles/clusterer_static.dir/src/devices/current_device.cpp.o"
+  "CMakeFiles/clusterer_static.dir/src/devices/current_device.cpp.o.d"
+  "CMakeFiles/clusterer_static.dir/src/devices/current_system.cpp.o"
+  "CMakeFiles/clusterer_static.dir/src/devices/current_system.cpp.o.d"
+  "CMakeFiles/clusterer_static.dir/src/utils.cpp.o"
+  "CMakeFiles/clusterer_static.dir/src/utils.cpp.o.d"
+  "libclusterer_static.a"
+  "libclusterer_static.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/clusterer_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
